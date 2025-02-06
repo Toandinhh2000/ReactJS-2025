8d7a45ea0bafe0c5179cd0ca609d187eef67b703
Toan Dinh <taplamtiktok686@gmail.com>
2025-02-06 09:35:14 +0700

    #16 Two-Way Binding

--- Bai 5 Props la gi/App.jsx
+++ Bai 5 Props la gi/App.jsx
@@ -1,18 +1,39 @@
+import { useState } from "react";
 import React {useState} from "react";
-
 function App() {
+//const  colors = ["#FF0000", "#0000FF", "#FF00FF"];
+//const [color, setColor] = useState("#FF0000");
 
-const [count, setCount] = useState("day la initial state");
+// const handleChangeColor = (index) => {
+// setColor(colors[index]);
+// };
 
-const handleCount = () => {
-setCount(count + 1)
+const[chooseProduct, setChooseProduct] = useState();
+
+const products = [
+  {id: 1, name: "iphone 13 pro"}
+  {id: 2, name: "iphone 14 pro"}
+  {id: 3, name: "iphone 15 pro"}
+];
+
+const handleGetValueChheck =(id) => {
+  setChooseProduct(id);
 };
 
-return<div>
-  <div>Count {count}</div>
-  <button onClick={handleCount}>Click me</button>
-</div>;
-
-}
-
-export default App;
+return (
+  <div>
+    <div>
+      {products.map((product, index) => {
+        return (
+          <div key={index}>
+          <label>{product.name}</label>
+          <Input
+          type="radio"
+          value={product.id}
+          onChange={(e) =>handleGetValueChheck(e.target.value)}
+          checked={+chooseProduct === product.id}
+          />
+    </div>
+        );
+      })}
+      </div>
