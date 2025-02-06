43f1cdc79b23f3eb1840101244372ec8f02ef0ad
Toan Dinh <taplamtiktok686@gmail.com>
2025-02-06 10:17:34 +0700

    #18 thuat ngu mount - unmount

--- Bai 5 Props la gi/App.jsx
+++ Bai 5 Props la gi/App.jsx
@@ -1,39 +1,19 @@
 import { useState } from "react";
-import React {useState} from "react";
+import content from "./components/content";
+
 function App() {
-//const  colors = ["#FF0000", "#0000FF", "#FF00FF"];
-//const [color, setColor] = useState("#FF0000");
+  const[show,setShow] =useState(false);
 
-// const handleChangeColor = (index) => {
-// setColor(colors[index]);
-// };
+  const handleShow = () => {
+    setShow(!show);
+  };
 
-const[chooseProduct, setChooseProduct] = useState();
-
-const products = [
-  {id: 1, name: "iphone 13 pro"}
-  {id: 2, name: "iphone 14 pro"}
-  {id: 3, name: "iphone 15 pro"}
-];
-
-const handleGetValueChheck =(id) => {
-  setChooseProduct(id);
-};
-
-return (
-  <div>
+  return (
     <div>
-      {products.map((product, index) => {
-        return (
-          <div key={index}>
-          <label>{product.name}</label>
-          <Input
-          type="radio"
-          value={product.id}
-          onChange={(e) =>handleGetValueChheck(e.target.value)}
-          checked={+chooseProduct === product.id}
-          />
+      <button onClick={handleShow}>Show</button>
+      {show && <Content />}
     </div>
-        );
-      })}
-      </div>
+  );
+}
+
+export default App;
---
+++ Bai 5 Props la gi/content.jsx
@@ -0,0 +1,3 @@
+export default function content() {
+  return<div>This is content</div>;
+}
