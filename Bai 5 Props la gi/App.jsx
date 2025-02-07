8df05d7cdefdc97b10076a62d04025ba6aabe10b
Toan Dinh <taplamtiktok686@gmail.com>
2025-02-07 10:06:24 +0700

    #24 React.memo() HOC

--- Bai 5 Props la gi/App.jsx
+++ Bai 5 Props la gi/App.jsx
@@ -1,18 +1,27 @@
-import React, {useEffect, useRef} from "react"
-
+import React, {useEffect, useRef, useState} from "react"
+import content from "./components/content";
 function App() {
-  const divRef = useRef();
+  const [count, setCount] = useState(1);
+  const [count2, setCount2] = useState(1);
 
-  useEffect(() => {
-    console.log(divRef.current);
-  }, []);
+  const handleIncrease = () => {
+    setCount(count + 1);
+  };
+
+  const handleIncrease2 = () =>{
+    setCount2(count2 + 1);
+  };
+
+  console.log("re-render fnc cha");
 
   return (
-<div>
-<div ref={divRef}>hello useRef</div>
-</div>
+    <div>
+      <div>{count}</div>
+      <button onClick={handleIncrease}>Click me</button>
+      <button onClick={handleIncrease2}>Click me 2</button>
+
+      <content count={count} />
+    </div>
   );
 }
-
-
 export default App;
---
+++ Bai 5 Props la gi/components/content.jsx
@@ -0,0 +1,6 @@
+function content () {
+  return <div>hello cac ban</div>
+}
+
+
+export default content
